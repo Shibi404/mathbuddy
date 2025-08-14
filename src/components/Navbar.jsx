a89df4d83831f370b9c26f8a0391b05cd54831cf
Shibi404 <shibisbs123@gmail.com>
2025-08-14 17:36:23 +0530

    MathBuddy: Make Navbar responsive for mobiles with dropdown menu

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,15 +1,34 @@
+import { useState } from 'react';
 import { Link } from 'react-router-dom';
 import '../styles/Navbar.css';
 
 export default function Navbar() {
+  const [isOpen, setIsOpen] = useState(false);
+
+  const toggleMenu = () => {
+    setIsOpen(!isOpen);
+  };
+
   return (
     <nav className="navbar">
       <div className="logo">MathBuddy</div>
-      <div className="nav-links">
-        <Link to="/">Home</Link>
-        <Link to="/learn">Learn</Link>
-        <Link to="/play">Play</Link>
-        <Link to="/quiz">Quiz</Link>
+
+      {/* Hamburger Icon */}
+      <div
+        className={`hamburger ${isOpen ? 'active' : ''}`}
+        onClick={toggleMenu}
+      >
+        <span></span>
+        <span></span>
+        <span></span>
+      </div>
+
+      {/* Nav Links */}
+      <div className={`nav-links ${isOpen ? 'open' : ''}`}>
+        <Link to="/" onClick={() => setIsOpen(false)}>Home</Link>
+        <Link to="/learn" onClick={() => setIsOpen(false)}>Learn</Link>
+        <Link to="/play" onClick={() => setIsOpen(false)}>Play</Link>
+        <Link to="/quiz" onClick={() => setIsOpen(false)}>Quiz</Link>
       </div>
     </nav>
   );
